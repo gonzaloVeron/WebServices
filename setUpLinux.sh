9e75f80fa0c189005fbaa349eb3c2408ba8c41f5
GonzaloVeron <gonveron96@gmail.com>
2020-04-28 16:31:44 -0300

    script para linux

---
+++ setUpLinux.sh
@@ -0,0 +1,86 @@
+echo "Script iniciado"
+
+node main.js addArtist OzzyOsbourne Inglaterra
+node main.js addArtist IronMaiden Inglaterra
+node main.js addArtist Oconnor Argentina
+node main.js addArtist JudasPriest Inglaterra
+node main.js addArtist ACDC Australia
+
+node main.js addAlbum 0 BlizzardOfOzz 1980
+node main.js addAlbum 0 DiaryOfAMadman 1981
+node main.js addAlbum 0 BarkAtTheMoon 1983
+node main.js addAlbum 1 PowerSlave 1983
+node main.js addAlbum 1 TheBookOfSouls 2015
+node main.js addAlbum 2 EstamosPariendo 2006
+node main.js addAlbum 2 RioExtraño 2010
+node main.js addAlbum 2 HayUnLugar 1999
+node main.js addAlbum 2 YerbaMalaNuncaMuere 2000
+node main.js addAlbum 2 UnPocoDeRespeto 2012
+node main.js addAlbum 3 PainKiller 1990
+
+node main.js addTrack 0 IDontKnow 55 Rock Metal
+node main.js addTrack 0 CrazyTrain 50 Rock Heavy
+node main.js addTrack 0 Dee 15 Rock
+node main.js addTrack 0 MrCrowley 75 Rock Heavy
+node main.js addTrack 1 OverTheMountain 61 Rock Metal Heavy
+node main.js addTrack 1 FlyingHighAgain 9 Rock
+node main.js addTrack 1 Believer 88 Metal
+node main.js addTrack 2 CentreOfEternity 88 Metal
+node main.js addTrack 2 SlowDown 88 Rock Heavy
+
+node main.js addTrack 3 2MinutesToMidnight 105 Heavy
+node main.js addTrack 3 LosferWords 510 Rock
+node main.js addTrack 3 TheDuellists 210 Rock Heavy Metal
+node main.js addTrack 4 SpeedOfLight 54 Metal
+node main.js addTrack 4 TheGreatUnknown 33 Metal
+
+node main.js addTrack 5 HastaSerLibre 54 Trash Rock
+node main.js addTrack 5 SabenBien 21 Rock Heavy Death
+node main.js addTrack 5 AlgoDeMi 66 Black Doom
+node main.js addTrack 6 Discordia 771 Doom 
+node main.js addTrack 6 LaMaldad 41 Power Heavy
+node main.js addTrack 7 Reflexion 28 Metal
+node main.js addTrack 8 Canibal 91 Black Rock
+node main.js addTrack 8 CortaVida 55 Metal
+node main.js addTrack 8 LaGran7 2 Metal
+node main.js addTrack 8 AtrapadoEnLaRed 75 Doom Black Power Death
+node main.js addTrack 8 InserviblePorDecreto 99 Heavy
+node main.js addTrack 8 LaSopaDelDiablo 13 Heavy
+node main.js addTrack 9 NoSoyUnExtraño 49 Rock
+node main.js addTrack 9 HombresDeHierro 101 Rock Doom
+
+node main.js addTrack 10 HellPatrol 102 Death Rock
+node main.js addTrack 10 LeatherRebel 765 Heavy
+node main.js addTrack 10 MetalMeltdown 145 Power
+
+node main.js createPlaylist HeavyMetal 350 Heavy Metal
+node main.js createPlaylist Doomed 1500 Doom
+node main.js createPlaylist AllSongs 350 Heavy Rock Metal Doom Death Power Death Trash
+node main.js createPlaylist BestRock 9999 Rock
+
+node main.js addUser Spore
+
+node main.js hear 0 30
+node main.js hear 0 17
+node main.js hear 0 2
+node main.js hear 0 5
+node main.js hear 0 9
+node main.js hear 0 22
+node main.js hear 0 17
+node main.js hear 0 9
+node main.js hear 0 9
+node main.js hear 0 9
+node main.js hear 0 9
+node main.js hear 0 2
+node main.js hear 0 17
+node main.js hear 0 22
+node main.js hear 0 22
+node main.js hear 0 17
+node main.js hear 0 30
+node main.js hear 0 30
+node main.js hear 0 17
+node main.js hear 0 2
+node main.js hear 0 17
+node main.js hear 0 17
+
+echo "Script finalizado"
